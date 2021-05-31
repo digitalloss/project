223420adae97d1b1a110708461de4308715cfde0
Eduard <example@gmil.com>
2021-05-31 07:36:36 +0300

    first films work

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 "use strict";
 
-console.log(2 + 2 * 2 );
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false
+};
 
+const a = prompt('Один из последних просмотренних фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Сколько фильмов вы уже просмотрели?'),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(++incr);
-console.log(--decr);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(5%2);
-
-console.log(2 + 2 * 2 !== '6');
-
-const isChecked = false,
-      isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(personalMovieDB);
